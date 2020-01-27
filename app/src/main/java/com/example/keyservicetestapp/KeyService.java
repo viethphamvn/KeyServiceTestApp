e0aedc4e1ea1839b58384fae98b65baac3af2230
Viet Pham <tug36669@temple.edu>
2020-01-27 12:47:23 -0500

    Added required methods for KeyService

--- app/src/main/java/com/example/keyservicetestapp/KeyService.java
+++ app/src/main/java/com/example/keyservicetestapp/KeyService.java
@@ -2,15 +2,47 @@ package com.example.keyservicetestapp;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
+import java.security.KeyPair;
+import java.security.interfaces.RSAPublicKey;
+
 public class KeyService extends Service {
+    IBinder myBinder = new MyBinder();
+
     public KeyService() {
+    }
+
+    public class MyBinder extends Binder {
+        KeyService getService(){
+            return KeyService.this;
+        }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return myBinder;
+    }
+
+    public KeyPair getMyKeyPair(){
+        return;
+    }
+
+    public void storePublicKey(String partnerName, String publicKey){
+
+    }
+
+    public RSAPublicKey getPublicKey(String partnerName){
+        return;
+    }
+
+    public void resetMyKeyPair(){
+
+    }
+
+    public void resetKey(String partnerName){
+
     }
 }
